e42afcb4f85431f38900d3f0a426952700bc2ef1
DEEPAK7525VERMA <145926307+DEEPAK7525VERMA@users.noreply.github.com>
2026-01-08 23:01:57 +0530

    first commit

---
+++ hospitalmanagement/src/test/java/com/deepak/twitch/hospitalmanagement/HospitalmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.twitch.hospitalmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
