29ceed33406e8b66d48d79a4905dd54cf6ab5743
DEEPAK7525VERMA <145926307+DEEPAK7525VERMA@users.noreply.github.com>
2026-01-10 20:32:12 +0530

    patient entity added

---
+++ hospitalmanagement/src/main/java/com/deepak/twitch/hospitalmanagement/Entity/Patient.java
@@ -0,0 +1,17 @@
+package com.deepak.twitch.hospitalmanagement.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate birthDate;
+    private String email;
+    private String gender;
+}
