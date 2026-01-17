cff8f2232e74fa4c308c213d75820c366158a545
DEEPAK7525VERMA <145926307+DEEPAK7525VERMA@users.noreply.github.com>
2026-01-17 23:57:59 +0530

    @Test

--- hospitalmanagement/src/test/java/com/deepak/twitch/hospitalmanagement/PatientTests.java
+++ hospitalmanagement/src/test/java/com/deepak/twitch/hospitalmanagement/PatientTests.java
@@ -1,7 +1,17 @@
 package com.deepak.twitch.hospitalmanagement;
 
+import com.deepak.twitch.hospitalmanagement.repository.PatientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PatientTests {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Test
+    public void testPatientRepository(){
+        patientRepository.findAll();
+    }
 }
